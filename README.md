b3ccd53244e067013a1910c657ce57f24d9f0081
Miroslav Tynovsky <tynovsky@avast.com>
2015-04-23 17:04:53 +0200

    initial commit: minilla scaffold

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Log::Dispatch::Gelf - It's new $module
+
+# SYNOPSIS
+
+    use Log::Dispatch::Gelf;
+
+# DESCRIPTION
+
+Log::Dispatch::Gelf is ...
+
+# LICENSE
+
+Copyright (C) Miroslav Tynovsky.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Miroslav Tynovsky <tynovsky@avast.com>
---
+++ lib/Log/Dispatch/Gelf.pm
@@ -0,0 +1,39 @@
+package Log::Dispatch::Gelf;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Log::Dispatch::Gelf - It's new $module
+
+=head1 SYNOPSIS
+
+    use Log::Dispatch::Gelf;
+
+=head1 DESCRIPTION
+
+Log::Dispatch::Gelf is ...
+
+=head1 LICENSE
+
+Copyright (C) Miroslav Tynovsky.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Miroslav Tynovsky E<lt>tynovsky@avast.comE<gt>
+
+=cut
+
